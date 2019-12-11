cdeebf9b4301be5021a76b00c21306136d137117
Glitch (hello-express) <none>
2019-12-11 20:38:40 +0000

    🐃⏲ Checkpoint ./config.js:8374370/186

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    TOKEN: 'NDY1NTY5MzgzNDI3NjA0NDgw.XfE5AA.qjadWeDRQqoFZgePeNt_89g30aIs',   
+    TOKEN: 'token',   
     YT_API_KEY: 'api ',
     prefix:  '=',
     devs: ['335645388323160064']
 }
-// https://اسم البروجكت.glitch.me/
