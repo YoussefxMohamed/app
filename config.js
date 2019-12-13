b34096988431d82b9e7ada80b12a6484fc2ce207
Glitch (hello-express) <none>
2019-12-13 01:07:20 +0000

    🌱💵 Checkpoint ./server.js:8374370/37 ./config.js:8374370/3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "NjU0Nzk1NjMxODM5NDEyMjI0.XfKwXg.VBf0VPIP3JxgtrExVg40yYdXOgo", //تعديل مهم توكن بوتك
+  TOKEN: "NjU0Nzk1NjMxODM5NDEyMjI0.XfKwXg.VBf0VPIP3JtxgtrExVg40yYdXOgo", //تعديل مهم توكن بوتك
   YT_API_KEY: "api ",
   prefix: "=",
   devs: ["335645388323160064"] ///تعديل مهم ايدي حسابك
