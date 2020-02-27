8bb92ddf24ba523fca49079442f06e3173569014
Glitch (hello-express) <none>
2020-02-27 21:31:01 +0000

    🌜⛄️ Checkpoint ./config.js:8374370/118 ./server.js:8374370/195

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "Njc0Njg4MjAxNTcwNTE2OTk0.Xle19w.YoiEvFS39cpXtE0Im-Yh7eMEkAg", //تعديل مهم توكن بوتك
+  TOKEN: "Njc0Njg4MjAxNTcwNTE2OTk0.XlgzwQ.oaa0Jpvw-obzsE5nMRKhmOHKrPk", //تعديل مهم توكن بوتك
   YT_API_KEY: "api ",
   prefix: "=",
   devs: ["335645388323160064"] ///تعديل مهم ايدي حسابك
