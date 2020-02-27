dee3ec1c88d8e14c1cefa7f97a49fdb3f415c73e
Glitch (hello-express) <none>
2020-02-27 22:29:12 +0000

    👆🍝 Checkpoint ./server.js:8374370/1062

--- server.js
+++ server.js
@@ -4326,7 +4326,7 @@ client.on('message', async message => {
 
     }
 })
-///كود البوتات
+///كود منشن بوتات
 
 client.on("message", message => {
 if(message.content === "#ls"){
@@ -4339,6 +4339,33 @@ list_all.push(`<@${bb.user.id}>`)
 }
 });
 
+////كود رابط
+
+
+ client.on('message', message => {
+    if (message.content.startsWith("$رابط")) {
+        message.channel.createInvite({
+        thing: true,
+        maxUses: 10,
+        maxAge: 3600,
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+    const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+          .setDescription("تم أرسال الرابط برسالة خاصة")
+           .setAuthor(client.user.username, client.user.avatarURL)
+                 .setAuthor(client.user.username, client.user.avatarURL)
+                .setFooter('طلب بواسطة: ' + message.author.tag)
+
+      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
+              const Embed11 = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        
+    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 1 **")
+      message.author.sendEmbed(Embed11)
+    }
+}); 
 
 //كود الفويس اونلاين
 let vojson = JSON.parse(fs.readFileSync('vojson.json', 'utf8')) // ملف تخزين الفويس اونلاين
