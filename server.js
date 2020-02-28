b6a49416b62ebfdefc402e587020fad35bb08107
Glitch (hello-express) <none>
2020-02-28 17:28:33 +0000

    🍿📍 Checkpoint ./server.js:8374370/5365

--- server.js
+++ server.js
@@ -3916,4 +3916,47 @@ client.on('voiceStateUpdate', (oldMember , newMember) => {
                 }
       });
 
+//// تعديل مهم الحالة والاسم والصورة
+client.on('ready',  () => {
+  console.log("ffff");
+});
+
+const developers = ['335645388323160064'] //الايدي هنا
+const adminprefix = "="//برفكس
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'pl')) {
+    client.user.setGame(argresult);
+      message.channel.send("**:white_check_mark: | The Playing Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send("**:white_check_mark: | The Watching Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send("**:white_check_mark: | The Listening Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/Rabea-YassiN");
+      message.channel.send("**:white_check_mark: | The Streaming Status Has Been Changed To : ``"
+   + `${argresult}` + "``**")
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+
 /// تعديل مهم هذا فقط تنبيه تم حذف الاكواد المتكررة والاكواد الخاطئة وتم اضافة تنبيهات مثل الميوزك وروم الهاك لوج تم حذف تغير ايدي سيرفرك
