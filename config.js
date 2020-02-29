6408a9ffb06d53d89e73449fc6b028a2bcd2e7b1
Glitch (hello-express) <none>
2020-02-29 09:46:55 +0000

    🌟🐆 Checkpoint ./config.js:8374370/154 ./server.js:8374370/3787

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  TOKEN: "NjgyOTQ3NzcwMDg4MjkyMzgw.Xlkc1g.4sSImEo8eemRtvt5inctwxfyQQs", //تعديل مهم توكن بوتك
+  TOKEN: "NjgzMjQ2OTk2OTQ0OTc3OTI0.Xloxag.VaNYexvJIN9AneWCI5X68FdZLxA", //تعديل مهم توكن بوتك
   YT_API_KEY: "api ",
   prefix: "=",
-  devs: ["335645388323160064"] ///تعديل مهم ايدي حسابك
+  devs: ["508723875253583896"] ///تعديل مهم ايدي حسابك
 };
--- server.js
+++ server.js
@@ -1205,7 +1205,6 @@ client.on("message", message => {
 **  =move •  لسحب الشخص الى روومك**
 **  =giveaway •   يسويلك قف اوي علي الشي الي تبيه**
 **  #bc •  لعمل برودكاست**
-**  =bc •  لعمل برودكاست - احتياط**
 
 `);
   }
@@ -3971,111 +3970,6 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
-
-////تعديل مهم برودكاست 
-
-
-client.on("message", async message => {
-  if (!message.guild || message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-  if (message.content.startsWith(prefix + "bc")) {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return;
-    let args = message.content
-      .split(" ")
-      .slice(1)
-      .join(" ");
-    message.channel
-      .send(
-        ">>> **[1] جميع الاعضاء\n[2] الاعضاء المتصلين\n[3] الرتب الخاصة\n[0] الغاء الأمر**"
-      )
-      .then(m => {
-        message.channel
-          .awaitMessages(msg => msg.author.id === message.author.id, {
-            max: 1,
-            time: 1000 * 60 * 2,
-            errors: ["time"]
-          })
-          .then(c => {
-            if (c.first().content === "1") {
-              message.guild.members.forEach(m => {
-                m.send(`${args}\n`).catch(err => {
-                  if (err) throw err;
-                });
-              });
-              c.first().delete();
-              m.delete();
-              message.channel.send("**تم نشر الرسالة بنجاح**");
-            }
-            if (c.first().content === "2") {
-              message.guild.members
-                .filter(m => m.presence.status !== "offline")
-                .forEach(m => {
-                  m.send(`${args}\n`).catch(err => {
-                    if (err) throw err;
-                  });
-                });
-              c.first().delete();
-              m.delete();
-              message.channel.send("**تم نشر الرسالة بنجاح**");
-            }
-            if (c.first().content == "0") {
-              c.first().delete();
-              m.delete();
-              message.channel.send("**تم الغاء الامر بنجاح**");
-            }
-            if (c.first().content === "3") {
-              m.edit("**>>> ادخل اسم الرتبة من فضلك**").then(ms => {
-                message.channel
-                  .awaitMessages(msg => msg.author.id === message.author.id, {
-                    max: 1,
-                    time: 1000 * 60 * 2,
-                    errors: ["time"]
-                  })
-                  .then(c => {
-                    let role = message.guild.roles.find(
-                      role => role.name === c.first().content
-                    );
-                    if (!role)
-                      return message.channel
-                        .send("**:x: لا استطيع العثور على الرتبة الخاصة بالرسالة**")
-                        .then(() => {
-                          ms.delete();
-                          c.first().delete();
-                        });
-                    let roleID = role.id;
-                    message.guild.roles.get(roleID).members.forEach(m => {
-                      m.send(`${args}\n`).catch(err => {
-                        if (err) throw err;
-                      });
-                    });
-                    c.first().delete();
-                    m.delete();
-              message.channel.send("**تم نشر الرسالة بنجاح**");
-                  });
-              });
-            }
-          })
-          .catch(() => m.delete());
-      });
-  } else if (message.content.startsWith(prefix + "seeeetname")) {
-    let args = message.content
-      .split(" ")
-      .slice(1)
-      .join(" ");
-    if (!message.author.id === "335645388323160064") return;
-    client.user.setUsername(args);
-    message.channel.send(`تم تغيير الاسم الى ..**${args}** `);
-  } else if (message.content.startsWith(prefix + "seeeetavatar")) {
-    let args = message.content
-      .split(" ")
-      .slice(1)
-      .join(" ");
-    if (!message.author.id === "335645388323160064") return;
-    client.user.setAvatar(args).catch(err => message.reply("send a valid url"));
-    message.channel.send(`تم تغيير الصورة الى :**${args}** `);
-  }
-});
-
 
 
 /// تعديل مهم هذا فقط تنبيه تم حذف الاكواد المتكررة والاكواد الخاطئة وتم اضافة تنبيهات مثل الميوزك وروم الهاك لوج تم حذف تغير ايدي سيرفرك
