7321213baf08d228694ed5c668c154cd96c356dd
Glitch (hello-express) <none>
2020-02-29 13:13:46 +0000

    🕍🏔 Checkpoint ./config.js:8374370/24

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   TOKEN: "NjgzMjQ2OTk2OTQ0OTc3OTI0.Xloxag.VaNYexvJIN9AneWCI5X68FdZLxA", //تعديل مهم توكن بوتك
-  YT_API_KEY: "api ",
   prefix: "=",
   devs: ["508723875253583896"] ///تعديل مهم ايدي حسابك
 };
