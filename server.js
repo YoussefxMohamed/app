b4f1e1836ce9fbd0755508ae5e0fbff8c5076987
Glitch (systemv2ok) <none>
2021-12-28 19:41:06 +0000

    📒💓 Checkpoint ./server.js:40324323/81

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", (request, response) => {
 });
 app.listen(process.env.PORT);
 setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+  http.get(`http://turbo-bot-finsal.glitch.me/`);
 }, 280000);
 
 ////بكجات
