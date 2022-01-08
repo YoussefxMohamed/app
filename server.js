982fc6011ee2ba25137b5a2d6492f075a3c952dd
Glitch (systemv2ok) <none>
2022-01-08 05:17:26 +0000

    👡🎺 Checkpoint ./server.js:40324323/786

--- server.js
+++ server.js
@@ -271,21 +271,15 @@ client.on("message", message => {
     message.guild.member(user).ban(7, user);
 
     const banembed = new Discord.RichEmbed()
-      .setAuthor(`BANNED!`, user.displayAvatarURL)
+	.setDescription('**:white_check_mark: ${user.tag} banned from the server ! :airplane: **')
       .setColor("RED")
       .setTimestamp()
-      .addField("**User:**", "**[ " + `${user.tag}` + " ]**")
-      .addField("**By:**", "**[ " + `${message.author.tag}` + " ]**")
-    	.setImage('https://i.imgur.com/AfFp7pu.png')
+    	.setImage('https://i.imgur.com/pgXofXt.gif')
     message.channel.send({
       embed: banembed
     });
 
-    message.channel.send(
-      `**:white_check_mark: ${user.tag} banned from the server ! :airplane: **
-https://cdn.glitch.global/68e463c8-a27e-44d3-8dfb-33a5b38bf9aa/Ban.gif?v=1641616939730  `
-     
-    );
+/// كود الانفايت
   }
 });
 client.on("message", async message => {
