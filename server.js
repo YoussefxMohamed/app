4e5281fd32ccbd0b6f9a55dd61b243b4c20b58a4
Glitch (systemv2ok) <none>
2022-01-20 13:57:40 +0000

    🏪💠 Checkpoint ./server.js:40324323/2560

--- server.js
+++ server.js
@@ -370,7 +370,7 @@ client.on("message", (message) => {
     message.guild.member(user).kick();
 
     const kickembed = new Discord.RichEmbed()
-      .setAuthor(`KICKED!`, user.displayAvatarURL)
+      .setAuthor(`KICKED <a:rks:933077528933990401>`, user.displayAvatarURL)
       .setColor("RANDOM")
       .setTimestamp()
       .addField("**User:**", "**[ " + `${user.tag}` + " ]**")
@@ -391,7 +391,7 @@ client.on("message", (message) => {
       .then((user) => {
         avt = user;
         let avtEmbed = new Discord.RichEmbed()
-          .setColor("#36393e")
+          .setColor("#03A9F4")
           .setAuthor(`${avt.username}'s Avatar`, message.author.avatarURL)
           .setImage(avt.avatarURL)
           .setFooter(`Avatar`, message.client.user.avatarURL);
@@ -4855,3 +4855,79 @@ Mobile | لو انت موبايل سهله اهي | :mobile_phone:
     embed17.channel.send(bot);
   }
 });
+
+
+
+/// TEST
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
+const db = require("quick.db")
+
+
+
+const prefix = "!"
+
+client.on("message", async message => {
+  if(message.content.startsWith(prefix + "react")){
+    const args = message.content.split(" ");
+    if(!args[1]) return message.channel.send("Please include an emoji");
+    if(!args[2]) return message.channel.send("There must be an id for the message");
+    if(isNaN(args[2])) return message.channel.send("Please include a valid message id");
+    if(!args[3]) return message.channel.send("Please include the id of the role ID");
+    if(isNaN(args[3])) return message.channel.send("Please include a valid ID for the role that should be given upon reaction.");
+    let emoji = ReactionEmojiGrab(args[1]);
+    if(!isNaN(emoji)) emoji = client.emojis.cache.get(emoji);
+    try{
+      const msg = await message.channel.messages.fetch(args[2]);
+      await msg.react(emoji);
+      
+      db.push(`${message.guild.id}.reactionroles`, 
+        {
+          message: msg.id,
+          emoji: emoji.id || emoji,
+          role: args[3]
+        }
+      );
+    }catch(e){
+      message.channel.send("Error\n" + e);
+    }
+  }
+})
+
+client.on("messageReactionAdd", (reaction, user) => {
+  if(user.bot) return;
+  const reactionRoles = db.get(`${reaction.message.guild.id}.reactionroles`);
+  if(!reactionRoles) return;
+  reactionRoles.forEach(async reactionRole => {
+    if((reactionRole.emoji === reaction._emoji.name || reactionRole.emoji == reaction._emoji.id) && reactionRole.message == reaction.message.id){
+      try{
+        await reaction.message.guild.members.cache.get(user.id).roles.add(reactionRole.role)
+      }catch(e){
+        console.log(e);
+      }
+    }
+  });
+})
+
+client.on("messageReactionRemove", (reaction, user) => {
+  if(user.bot) return;
+  const reactionRoles = db.get(`${reaction.message.guild.id}.reactionroles`);
+  if(!reactionRoles) return;
+  reactionRoles.forEach(async reactionRole => {
+    if((reactionRole.emoji === reaction._emoji.name || reactionRole.emoji == reaction._emoji.id) && reactionRole.message == reaction.message.id){
+      try{
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(reactionRole.role)
+      }catch(e){
+        console.log(e);
+      }
+    }
+  });
+})
+
+function ReactionEmojiGrab(reactionArg){
+  const contents = reactionArg.substring(1, reactionArg.length - 1).split(":");
+  if(contents.length > 1){
+    return contents[2];
+  }else{
+    return reactionArg;
+  }
+}
