e185b3054c0dcc12bf65264493bbcc4523d4b0af
Glitch (systemv2ok) <none>
2022-01-25 09:06:07 +0000

    🍳⛸ Checkpoint ./server.js:40324323/2468

--- server.js
+++ server.js
@@ -4989,3 +4989,72 @@ client.on('message', nibo => {
 .setFooter(`${client.user.username}`)
 nibo.channel.send(embed);
 }});
+
+/*
+//// Reaction Role
+const db = require("quick.db")
+client.on("message", async message => {
+  if(message.content.startsWith(prefix + "react")){
+    const args = message.content.split(" ");
+    if(!args[1]) return message.channel.send("Please include an emoji");
+    if(!args[2]) return message.channel.send("There must be an id for the message");
+    if(isNaN(args[2])) return message.channel.send("Please include a valid message id");
+    if(!args[3]) return message.channel.send("Please include the id of the role ID");
+    if(isNaN(args[3])) return message.channel.send("Please include a valid ID for the role that should be given upon reaction.");
+    let emoji = ReactionEmojiGrab(args[1]);
+    if(!isNaN(emoji)) emoji = client.emojis.get(emoji);
+    try{
+      const msg = await message.channel.fetchMessage(args[2]);
+      await msg.react(emoji);
+      
+      db.push(`${message.guild.id}.reactionroles`, 
+        {
+          message: msg.id,
+          emoji: emoji.id || emoji,
+          role: args[3]
+        }
+      );
+    }catch(e){
+      message.channel.send("Error\n" + e);
+    }
+  }
+})
+
+client.on("messageReactionAdd", (reaction, user) => {
+  if(user.bot) return;
+  const reactionRoles = db.get(`${reaction.message.guild.id}.reactionroles`);
+  if(!reactionRoles) return;
+  reactionRoles.forEach(async reactionRole => {
+    if((reactionRole.emoji === reaction._emoji.name || reactionRole.emoji == reaction._emoji.id) && reactionRole.message == reaction.message.id){
+      try{
+        await reaction.message.guild.members.get(user.id).roles.add(reactionRole.role)
+      }catch(e){
+        console.log(e);
+      }
+    }
+  });
+})
+
+client.on("messageReactionRemove", (reaction, user) => {
+  if(user.bot) return;
+  const reactionRoles = db.get(`${reaction.message.guild.id}.reactionroles`);
+  if(!reactionRoles) return;
+  reactionRoles.forEach(async reactionRole => {
+    if((reactionRole.emoji === reaction._emoji.name || reactionRole.emoji == reaction._emoji.id) && reactionRole.message == reaction.message.id){
+      try{
+        await reaction.message.guild.members.cache.get(user.id).roles.remove(reactionRole.role)
+      }catch(e){
+        console.log(e);
+      }
+    }
+  });
+})
+
+function ReactionEmojiGrab(reactionArg){
+  const contents = reactionArg.substring(1, reactionArg.length - 1).split(":");
+  if(contents.length > 1){
+    return contents[2];
+  }else{
+    return reactionArg;
+  }
+}*/
